f6f481e4f5d29f6b2aa431110c051e804269e132
Lgamesdev <louisgamesdev@gmail.com>
2023-08-12 18:46:33 +0200

    first commit player module

---
+++ PlayerModule/PlayerModule.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Services.CloudCode.Core;
+
+namespace PlayerModule;
+public class PlayerModule
+{
+    [CloudCodeFunction("SayHello")]
+    public string Hello(string name)
+    {
+        return $"Hello, {name}!";
+    }
+}
